b7ecaaa076ef81610e8ded55b8029919e951ca77
thaivan <giacatluong20121996@gmail.com>
2020-08-19 19:53:25 +0700

    [PARALLAX SCROLLING PAGE]

---
+++ _simple_parallax_scrolling_effect/effect.js
@@ -0,0 +1,29 @@
+/**
+ *
+ * @author thai-van
+ *
+ **/
+
+let bg = document.getElementById("bg");
+let moon = document.getElementById("moon");
+let mountain = document.getElementById("mountain");
+let road = document.getElementById("road");
+let text = document.getElementById("text");
+
+window.addEventListener('scroll', function () {
+    let value = window.scrollY;
+
+    bg.style.top = toPx(value, 0.5);
+    moon.style.left = toPx(-value, 0.5);
+    mountain.style.top = toPx(-value, 0.15);
+    road.style.top = toPx(value, 0.15);
+    text.style.top = toPx(value, 0.15);
+
+    console.log("scrolling " + value);
+});
+
+function toPx(x, y) {
+    let value = x * y + 'px';
+    console.log(value);
+    return value;
+}
--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
-Creative Menu Hover Effects
-https://www.youtube.com/watch?v=TcPYVqHAHNQ
+####[Creative Menu Hover Effects](https://www.youtube.com/watch?v=TcPYVqHAHNQ)
+
+####[Top 20 CSS effects](https://www.youtube.com/watch?v=QNTeq4QdOsQ)
